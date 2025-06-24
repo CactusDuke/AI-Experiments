7e537090c6dc31450b831ea8456ae42a27cca725
Seth Close <seth@sethclose.au>
2025-06-24 16:14:11 -0600

    Added Con2d and Flatten and did slight cleanup of error logic

--- tools/network.py
+++ tools/network.py
@@ -10,26 +10,41 @@ class Network:
 
     def addLayers(self, layers):
         try:
-            self.inputSize = layers[0].inputSize
+            if layers[0].type == 2: #If start is 1d
+                self.inputSize = layers[0].inputSize
+            elif layers[0].type == 3: #If start is 2d
+                self.inputSize = layers[0].inputSizeY #Number of rowsa
             for i in range(len(layers)):
                 if i < (len(layers) - 1):
-                    if layers[i + 1].type == 1:
+                    if layers[i + 1].type == 0: #Base Layer Functions N -> N
+                        assert layers[i].outputSize != 0, f"Incorrect Output Format for layers {i}" #Checks that the correct output type exists
+                        assert layers[i].outputSize == layers[i + 1].inputSize, f"Input/Output values do not match for layer {i}"
+
+                    elif layers[i + 1].type == 1: #Activation functions N
+                        assert layers[i].outputSize != 0, f"Incorrect Output Format for layers {i}" #Checks that the correct output type exists
                         layers[i + 1].inputSize = layers[i].outputSize
                         layers[i + 1].outputSize = layers[i].outputSize
+                    
+                    elif layers[i + 1].type == 2: #Functions like Flatten. 2d -> 1d
+                        assert layers[i].outputSizeX != 0, f"Incorrect Output Format for layers {i}" #Checks that the correct output type exists
+                        layers[i + 1].inputSizeX = layers[i].outputSizeX
+                        layers[i + 1].inputSizeY = layers[i].outputSizeY
+                        layers[i + 1].outputSize = layers[i].outputSizeY * layers[i].outputSizeX
 
-                    assert layers[i].outputSize == layers[i + 1].inputSize, f"Input/Output values do not match for layer {i}"
+                    elif layers[i + 1].type == 3: #2d Functions R2 -> R2
+                        assert layers[i].outputSizeX != 0, f"Incorrect Output Format for layers {i}" #Checks that the correct output type exists
+                        assert layers[i].outputSizeX == layers[i + 1].inputSizeX, f"Input/Output X values do not match for layer {i}"
+                        assert layers[i].outputSizeY == layers[i + 1].inputSizeY, f"Input/Output Y values do not match for layer {i}"
+
                 self.layerList.append(layers[i])
         except Exception as error:
             print("Error: " + str(error))
-        
-    def fitNetwork(self):
-        pass
 
     def calculateOutput(self, inputV):
         try:
             assert len(inputV) == self.inputSize, "Invalid Input Size"
             for x in inputV:
-                assert type(x) == float or type(x) == int, f"Invalid Input Type: {x}"
+                assert type(x) != str, f"Invalid Input Type: {x}"
 
             for layer in self.layerList:
                 inputV = layer.run(inputV) #Passes input through layer and returns output as new input
@@ -79,7 +94,7 @@ class Linear:
         inputVals.append(inputV) #Although inputed as a 1d array it needs to be an (inputsize) X 1 matrix
         output = np.matmul(inputVals, self.weights) #Multiply weights by the values
         output = np.add(output, self.bias) #Add the biases
-        return(output) #Return the output
+        return(output[0]) #Return the output
 
 
     def printWeights(self):
@@ -126,3 +141,104 @@ class Sigmoid:
             output.append(1 / (1 + math.exp(-1 * i))) #Sigmoid of the valuse
         return(output)
 
+class LogSoftMax:
+    def __init__(self):
+        self.inputSize = 0
+        self.outputSize = 0
+        self.type = 1 #Determines if it is a layer, an activation function, etc
+        
+    def run(self, inputV):
+        output = []
+        total = 0
+        for i in inputV:
+            total += math.exp(i) #Adds value to total
+
+        for i in inputV:
+            output.append(float(math.log(math.exp(i)/total))) #Log of the exponent percentage
+        return(output)
+
+class SoftMax:
+    def __init__(self):
+        self.inputSize = 0
+        self.outputSize = 0
+        self.type = 1 #Determines if it is a layer, an activation function, etc
+        
+    def run(self, inputV):
+        output = []
+        total = 0
+        for i in inputV:
+            total += math.exp(i) #Adds value to total
+
+        for i in inputV:
+            output.append(float(math.exp(i) / total)) #Log of the exponent percentage
+        return(output)
+
+class Flatten:
+    def __init__(self):
+        self.inputSizeX = 0
+        self.inputSizeY = 0
+        self.outputSize = 0
+        self.type = 2 #Determines if it is a layer, an activation function, etc
+        
+    def run(self, inputV):
+        output = []
+        for row in inputV:
+            for value in row:
+                output.append(value) #Flattens 2d matric into 1d
+
+        return(output)
+
+class Con2D:
+    def __init__(self, inputSizeX, inputSizeY, kernal, stride, padding = 0):
+        assert len(kernal[0]) > 0, "Kernal must be 2d array"
+        assert len(kernal) > 0, "Kernal cannot be empty"
+
+
+        self.inputSizeX = inputSizeX
+        self.inputSizeY = inputSizeY
+        self.kernal = np.array(kernal)
+        self.stride = stride
+        self.padding = padding
+        self.type = 3 #Determines if it is a layer, an activation function, etc.
+
+        self.outputSizeX = int(((inputSizeX - len(kernal[0]) + (2 * padding)) / stride) + 1)
+        self.outputSizeY = int(((inputSizeY - len(kernal) + (2 * padding)) / stride) + 1)
+
+    
+    def run(self, inputV):      
+        #Add Padding
+        rowPad = [0] * (self.inputSizeX + (2 * self.padding)) #Empty row to append to the input to add padding
+        for row in inputV:
+            for p in range(self.padding):
+                row.insert(0,0) #Insert 0 into start of list
+                row.append(0) #append 0 to end of list
+        for p in range(self.padding): 
+            inputV.insert(0,rowPad) #Insert row padding into start of list
+            inputV.append(rowPad) #append row padding to end of list
+
+        #Make list into array
+        inputV = np.array(inputV) #Makes it into a numpy array. Allows for easier matrix splitting
+
+
+        #Calculation
+        startXcord = 0 #Top left value of kernal for each itertion. Used to move around
+        startYcord = 0
+        output = []
+        #tempMatrix[rowStart:rowEnd, colStart:colEnd]. Here to show what each value does
+        
+        for j in range(self.outputSizeY):
+            startXcord = 0
+            row = []
+            for i in range(self.outputSizeX):
+                #tempMatrix = inputV[startYcord:(startYcord + len(self.kernal[0])), startXcord:(startXcord + len(self.kernal))] #Temp matrix with the subset of the input that is being studied
+                tempMatrix = inputV[startYcord:(startYcord + len(self.kernal)), startXcord:(startXcord + len(self.kernal[0]))] #Temp matrix with the subset of the input that is being studied
+                #Fuckin dot product gets funky with 2d arrays
+                dotProd = 0
+                for i in range(len(self.kernal)):
+                    dotProd += np.dot(tempMatrix[i], self.kernal[i]) #Splits the 2d arrays into 1d ones and combines dot values. [[1, 2], [3, 4]] dot [[1, 1], [2, 2]] = 1*1 + 2*1 + 3*2 + 4*2 = [1, 2] dot [1, 1] + [3, 4] dot [2, 2]
+                row.append(float(dotProd)) #Append to row
+                startXcord += self.stride #Move to next start point
+
+            output.append(list(row))
+            startYcord += self.stride
+        return(output)
--- tools/testing.py
+++ tools/testing.py
@@ -1,5 +1,21 @@
 from network import *
 
+
+test = Network()
+test.addLayers([
+    Con2D(3, 3, [[1, -2]], 1),
+    Flatten(),
+    Linear(6, 2),
+    LogSoftMax()
+])
+
+
+
+
+print(test.calculateOutput([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+
+
+"""
 test = Network()
 test.addLayers([
     Linear(10, 7),
@@ -8,8 +24,52 @@ test.addLayers([
     ReLU()
 ])
 
-#print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-#test.printNetwork()
+#Calculation
+startXcord = 0 #Top left value of kernal for each itertion. Used to move 
+startYcord = 0
+inputV = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-print(test.calculateOutput([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+#Add Padding
+padding = 2
+rowPad = [0] * (3 + (2 * padding)) #Empty row to append to the input to add padding
+for row in inputV:
+    for p in range(padding):
+        row.insert(0,0) #Insert 0 into start of list
+        #np.insert(row, 0, 0)
+        row.append(0) #append 0 to end of list
+        #np.append(row, 0)
+for p in range(padding): 
+    inputV.insert(0,rowPad) #Insert row padding into start of list
+    #np.insert(inputV, 0, rowPad)
+    inputV.append(rowPad) #append row padding to end of list
+    #np.append(inputV, rowPad)
+
+inputV = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+inputV = np.array(inputV)
+
+#print(inputV)
+tempMatrix = inputV.copy()
+#print(tempMatrix[1:3, 0:3])
+#print(inputV)
+#print(tempMatrix[rowStart:rowEnd, colStart:colEnd])
+
+kernal = [[1, 2], [3, 4]]
+startYcord=0
+startXcord=0
+#tempMatrix[startYcord:(startYcord + len(kernal[0])), startXcord:(startXcord + len(kernal))]
+output = []
+for j in range(2):
+    startXcord=0
+    row = []
+    for i in range(2):
+        tempMatrix = inputV[startYcord:(startYcord + len(kernal[0])), startXcord:(startXcord + len(kernal))]
+        startXcord += 1
+        #Fuckin dot product gets funky with 2d arrays
+        dotProd = 0
+        for i in range(len(kernal)):
+            dotProd += np.dot(tempMatrix[i], kernal[i]) #Splits the 2d arrays into 1d ones and combines dot values. [[1, 2], [3, 4]] dot [[1, 1], [2, 2]] = 1*1 + 2*1 + 3*2 + 4*2 = [1, 2] dot [1, 1] + [3, 4] dot [2, 2]
+        row.append(int(dotProd)) #Append to row
+    output.append(list(row))
+    startYcord += 1
+print(output)
+"""
